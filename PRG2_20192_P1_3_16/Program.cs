7fedb3393f8263218de761b8fa20931943b452f8
Mochammad Reza Fahlevi <mochammadfahlevi01@gmail.com>
2020-04-28 19:25:17 +0700

    initial commit

---
+++ PRG2_20192_P1_3_16/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PRG2_20192_P1_3_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, N;
+            Console.WriteLine(" PROGRAM ANAK AYAM");
+            Console.Write(" Masukkan batasan : ");
+            N = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(" ----------------------------------------");
+
+            for (i = N; i > 0; i--)
+            {
+                if(i>1)
+                {
+                    Console.WriteLine(" Anak ayam turun {0}, mati satu tinggal {1}", i, i-1);
+                }
+                else if(i == 1)
+                {
+                    Console.WriteLine(" Anak ayam turun {0}, mati satu tinggal induknya", i);
+                }
+            }
+        }
+    }
+}
